ec4dab39e4c2b96219e13f50eee7d38a53f6ed29
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 17:38:56 -0400

    Create Checkbox.tsx

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { ForgotPasswordLink } from './styles';
+
+export default function BasicButtons() {
+  return (
+    <ForgotPasswordLink href="/forgot-password">
+      Forgot Password
+    </ForgotPasswordLink>
+  );
+}

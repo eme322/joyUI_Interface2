4081d10ae37dd064e04bd3208b3d18d1eefc5f9f
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 18:36:20 -0400

    Update Checkbox.tsx

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Checkbox as MuiCheckbox, FormControlLabel } from '@mui/material';
 
+// Define a type for your props
+type CheckboxProps = {
+  label: string;
+  checked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
 export default function Checkbox({ label, checked, onChange }) {
   return (
     <FormControlLabel

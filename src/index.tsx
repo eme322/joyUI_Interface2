4a08a4c6e0ecb3f6e8ba3adb5fe729517f2f17fd
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 17:28:25 -0400

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+// joyUI_Interface1/src /index.tsx
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
+import Demo from './Demo';
+import { globalStyles } from './components/styles';
+
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <CssVarsProvider>
+        {globalStyles()}
+        <Demo />
+      </CssVarsProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>
+);

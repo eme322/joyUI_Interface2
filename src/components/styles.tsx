9b26be7b06178e0c93a0df3373e1f70434cbf1b0
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 17:24:24 -0400

    Create styles.tsx

---
+++ src/components/styles.tsx
@@ -0,0 +1,33 @@
+// joyUI_Interface1/src/components/styles.tsx
+// styles.tsx
+import * as React from 'react';
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
+/*
+export const ForgotPasswordLink = styled.a`
+  color: #F21D6B;
+  text-decoration: none; // No underline by default
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline; // Underline on hover to indicate interactivity
+    color: darken(#D8819E, 10%);
+  }
+`;
+*/
+
+export const globalStyles = () => (
+  <Global
+    styles={css`
+      body {
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+      }
+      #logo {      
+  margin-top: 20px;
+  width: 100px; 
+  height: auto; 
+}
+    `}
+  />
+);

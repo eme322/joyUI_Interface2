5c99dc1d06c19863c9770a6a223f4ff52a866dd8
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 22:50:37 -0400

    Create LogInButton.tsx

---
+++ src/components/LogInButton.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+
+export default function BasicButtons() {
+  const handleClick = () => {
+    console.log("Button clicked!");
+    
+    
+  };
+
+  return (
+    <Button onClick={handleClick} sx={{ width: 450, backgroundColor: '#F21D6B', '&:hover': { backgroundColor: '#FF4081' } }}>Sign In</Button>
+  );
+}

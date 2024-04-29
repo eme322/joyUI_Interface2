a300abf7fd1915567a12eda29a1023bd40805859
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 21:41:31 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 // joyUI_Interface1/src /index.tsx
+import Demo from './Demo';
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
-import Demo from './Demo';
 import { globalStyles } from './components/styles';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(

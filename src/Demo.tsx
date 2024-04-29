f73b570c3ddea33b12c492b25239606841ecb273
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 22:51:00 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import Card from "@mui/joy/Card";
 import Typography from "@mui/joy/Typography";
+
 import Email from "./components/Email";
 import Password from "./components/Password";
 import AspectRatio from "@mui/joy/AspectRatio";

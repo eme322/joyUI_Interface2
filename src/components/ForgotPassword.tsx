99bf8e6c2a98f77a25a6efee4ec0780e336f3f6e
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-29 01:14:44 -0400

    Update ForgotPassword.tsx

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -1,4 +1,50 @@
 import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+
+export default function TermsAgreement() {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checked}
+            onChange={handleChange}
+            name="agreeToTerms"
+            color="primary"
+          />
+        }
+        label={
+          <React.Fragment>
+            I agree to the{' '}
+            <Link href="/terms" underline="hover">
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link href="/privacy-policy" underline="hover">
+              Privacy Policy
+            </Link>
+          </React.Fragment>
+        }
+      />
+    </div>
+  );
+}
+
+
+
+
+
+
+/*
+import * as React from 'react';
 import { ForgotPasswordLink } from './styles';
 
 export default function BasicButtons() {
@@ -8,3 +54,4 @@ export default function BasicButtons() {
     </ForgotPasswordLink>
   );
 }
+*/

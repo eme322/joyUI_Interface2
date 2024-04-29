b14b1a146e5a96527cb00746718e78eef4d47cf3
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-29 12:24:03 -0400

    Update Checkbox.tsx

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,61 +1,4 @@
-//checkbox.tsx
 import React from 'react';
-import { Checkbox, FormControlLabel, Link, Typography, ThemeProvider, createTheme } from '@mui/material';
-
-// Define a theme to ensure all custom properties are included
-const theme = createTheme({
-  components: {
-    MuiCheckbox: {
-      styleOverrides: {
-        root: {
-          '&:hover': {
-            backgroundColor: 'rgba(25, 118, 210, 0.04)' // You can adjust this as needed
-          }
-        }
-      }
-    }
-  }
-});
-
-function CheckboxAgrement() {
-  const [isChecked, setIsChecked] = React.useState(false);
-
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(event.target.checked);
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={isChecked}
-            onChange={handleCheckboxChange}
-            name="agreeToTerms"
-            color="primary"
-          />
-        }
-        label={
-          <Typography>
-            I agree to the{' '}
-            <Link href="/terms" underline="hover">
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link href="/privacy-policy" underline="hover">
-              Privacy Policy
-            </Link>.
-          </Typography>
-        }
-      />
-    </ThemeProvider>
-  );
-}
-
-export default CheckboxAgrement;
-
-
-/*import React from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from '@mui/material/Link';
@@ -94,6 +37,6 @@ function CheckboxAgrement() {
   );
 }
 
-export default CheckboxAgrement; */
+export default CheckboxAgrement;
 
       
